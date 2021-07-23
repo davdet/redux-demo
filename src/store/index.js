da88d4a83799e6df49487ba3feda830dcb1e36d0
davdet <dv.dt@hotmail.com>
2021-07-23 11:47:04 +0200

    store refactoring into multiple files

--- src/store/index.js
+++ src/store/index.js
@@ -1,63 +1,13 @@
-import { createSlice, configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 
-// stato iniziale per il counter
-const initialCounterState = { counter: 0, showCounter: true };
+import counterReducer from "./counter-slice";
+import authReducer from "./auth-slice";
 
-// creazione della state slice per il counter.
-// NB: utilizzando redux-toolkit è possibile modificare lo stato in una maniera che
-// sembrerebbe diretta, ma in realtà non lo è (è solo un'astrazione)
-const counterSlice = createSlice({
-  name: "counter",
-  initialState: initialCounterState,
-  reducers: {
-    increment(state) {
-      state.counter++;
-    },
-    increase(state, action) {
-      state.counter = state.counter + action.payload;
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    reset(state) {
-      state.counter = 0;
-    },
-    toggle(state) {
-      state.showCounter = !state.showCounter;
-    },
-  },
-});
-
-// stato iniziale per l'auth
-const initialAuthState = {
-  isAuthenticated: false,
-};
-
-// creazione della state slice per l'auth
-const authSlice = createSlice({
-  name: "authentication",
-  initialState: initialAuthState,
-  reducers: {
-    login(state) {
-      state.isAuthenticated = true;
-    },
-    logout(state) {
-      state.isAuthenticated = false;
-    },
-  },
-});
-
-// per fare il merge di più reducers in un unico reducer si usa la funzione configureStore messa a disposizione da redux-toolkit
 const store = configureStore({
   reducer: {
-    counter: counterSlice.reducer,
-    auth: authSlice.reducer,
+    counter: counterReducer,
+    auth: authReducer,
   },
 });
-
-// utilizzando redux-toolkit le actions vengono create automaticamente con un identificatore univoco,
-// e sono accessibili utilizzando le chiavi impostate nella sezione reducers di createSlice
-export const counterActions = counterSlice.actions;
-export const authActions = authSlice.actions;
 
 export default store;
